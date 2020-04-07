e55ae612d27dc572c4933d43bb7a90e9e3b78ed5
Samgeven <sam.geven@mail.ru>
2020-04-08 00:40:05 +0300

    started a script file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 // Custom colorpicker icon
-
 const inputColor = document.querySelectorAll('.main-input_color:after');
+
+
+// input selectors
+const input = document.querySelectorAll('input');
+const sidenoteWrp = document.getElementById('sideNote');
+const fontsize = document.getElementById('fontSize');
+
+
+function changeSidenote() {
+    sidenoteWrp.style.fontSize == fontsize.value + 'px';
+}
+
+changeSidenote();
+
+input.forEach(el => el.addEventListener('change', changeSidenote()));
